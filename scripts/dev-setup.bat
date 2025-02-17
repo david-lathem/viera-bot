78f633b6b868a6dd3afca3e07903d8c3f31f0028
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-02-17 21:12:59 +0500

    update: add build step in batch files

--- scripts/dev-setup.bat
+++ scripts/dev-setup.bat
@@ -1,4 +1,6 @@
 copy %~dp0..\.env.example %~dp0..\.env
 call npm i
 echo Setup complete
+echo Building JS files from TS
+npm run build
 pause
--- scripts/prod-setup.bat
+++ scripts/prod-setup.bat
@@ -2,5 +2,7 @@
 copy %~dp0..\.env.example %~dp0..\.env
 echo Downloading dependencies [prod only]
 call npm install --omit=dev
+echo Building JS files from TS
+npm run build
 echo Setup complete
 pause
